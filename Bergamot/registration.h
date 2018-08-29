a5fac33bdc179a37b5fb142f37a99b92e52b8d69
Aline <39874992+AlineCrawf@users.noreply.github.com>
2018-08-29 14:59:08 +0300

    Full registration

--- Bergamot/registration.h
+++ Bergamot/registration.h
@@ -18,8 +18,15 @@ public:
 private slots:
     void on_okButton_clicked();
 
+    void on_passwdEdit_textChanged(const QString &arg1);
+
+    void on_reppassEdit_textChanged(const QString &arg1);
+
+    void on_nameEdit_textChanged(const QString &arg1);
+
 private:
     Ui::Registration *ui;
+    QString to_hash(QString passwd);
 };
 
 #endif // REGISTRATION_H
